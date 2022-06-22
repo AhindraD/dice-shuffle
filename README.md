bbbb6f561561ec5b03bd0362c7f21ebb397eba1b
Ahindra D <83480142+AhindraD@users.noreply.github.com>
2022-06-22 11:00:28 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,14 @@
+# Dice Shuffle
+## Some Steps
+1. Create html css js files, link and check setup
+2. Write basic CSS for dice
+3. Write JS function `reset` to generate random numbers and calculate total, set the numbers in dice using DOM.
+4. Event listener on button, call `reset`
+5. Call `reset` on page load.
+
+
+![](screenshot.png)
+
+# Style Guide
+- Background color: rgba(192, 10, 46, 0.753);
+- Button background: #56101e;
---
+++ script.js
@@ -0,0 +1,70 @@
+function reset() {
+    let res1 = Math.ceil(Math.random() * 6);
+    let res2 = Math.ceil(Math.random() * 6);
+    let total = res1 + res2;
+
+    let totalElem = document.querySelector(".total");
+    totalElem.innerHTML = `<span class="total">Total : ${total}</span>`;
+
+    let roll1 = document.querySelector(".dice1");
+    let roll2 = document.querySelector(".dice2");
+    roll1.innerHTML = facesMap[res1];
+    roll2.innerHTML = facesMap[res2];
+}
+
+
+let facesMap = {
+    1: `<div class="one-face">
+    <span class="dot"></span>
+    </div>`,
+
+    2: `<div class="two-face">
+    <span class="dot"></span>
+    <span class="dot"></span>
+    </div>`,
+
+    3: `<div class="three-face">
+    <span class="dot"></span>
+    <span class="dot"></span>
+    <span class="dot"></span>
+    </div>`,
+
+    4: `<div class="four-face">
+    <div class="row">
+    <span class="dot"></span>
+    <span class="dot"></span>
+    </div>
+    <div class="row">
+    <span class="dot"></span>
+    <span class="dot"></span>
+    </div>
+    </div>`,
+
+    5: `<div class="five-face">
+    <div class="row">
+    <span class="dot"></span>
+    <span class="dot"></span>
+    </div>
+    <div class="row-mid">
+    <span class="dot"></span>
+    </div>
+    <div class="row">
+    <span class="dot"></span>
+    <span class="dot"></span>
+    </div>
+    </div>`,
+
+    6: `<div class="six-face">
+    <div class="row">
+    <span class="dot"></span>
+    <span class="dot"></span>
+    <span class="dot"></span>
+    </div>
+    <div class="row">
+    <span class="dot"></span>
+    <span class="dot"></span>
+    <span class="dot"></span>
+    </div>
+    </div>`,
+
+}
